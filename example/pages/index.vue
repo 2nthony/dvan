1a5b9e421206b122e5a81b1fc136fb8caf1239f3
evila <ijoec123@gmail.com>
2018-08-31 14:35:31 +0800

    example update

--- example/pages/index.vue
+++ example/pages/index.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="index">
     index
+    <router-link to="bar">bar</router-link>
     <router-link to="foo">foo</router-link>
     <router-link to="main/foo">main-foo</router-link>
+    <p>ssd</p>
   </div>
 </template>
 

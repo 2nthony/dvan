b436c563d914c00e10bd4df849225238e1b789c9
evila <ijoec123@gmail.com>
2018-11-04 22:33:32 +0800

    Set unnamed plugin-name with it's path

--- packages/dvan/lib/utils/loadPlugins.js
+++ packages/dvan/lib/utils/loadPlugins.js
@@ -5,12 +5,19 @@ const isLocalPlugin = p => /^[./]|(^[a-zA-Z]:)/.test(p)
 module.exports = (baseDir, plugins) => {
   return plugins.map(plugin => {
     if (plugin && typeof plugin === 'object') {
+      if (!plugin.name) plugin.name = 'Unnamed plugin from "dvan.config".plugins'
+
       return plugin
     }
+
     if (typeof plugin === 'string') {
-      return isLocalPlugin(plugin)
-        ? require(plugin)
+      const _plugin = isLocalPlugin(plugin)
+        ? require(require('upath').join(baseDir, plugin))
         : require(resolveFrom(baseDir, plugin))
+
+      if (!_plugin.name) _plugin.name = `Unnamed plugin from "${plugin}"`
+
+      return _plugin
     }
   })
 }

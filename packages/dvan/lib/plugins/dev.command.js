0e148e0bcb33f6d4b97ba39a8c3fa1173f31d69b
evila <ijoec123@gmail.com>
2018-11-10 02:37:00 +0800

    [dvan] Dev.command update

--- packages/dvan/lib/plugins/dev.command.js
+++ packages/dvan/lib/plugins/dev.command.js
@@ -9,6 +9,14 @@ exports.extend = api => {
     async () => {
       if (api.flags.help) return
 
+      api.chainWebpack(config => {
+        config
+          .entry('app')
+          .add('webpack-dev-server/client?/')
+          .add('webpack/hot/dev-server')
+        config.plugin('hmr').use(webpack.HotModuleReplacementPlugin)
+      })
+
       const { host, port } = api.config.devServer
       const actualPort = await require('get-port')({ host, port })
       const WebpackDevServer = require('webpack-dev-server')
@@ -28,19 +36,12 @@ exports.extend = api => {
       server.listen(actualPort, host)
     }
   )
-  command.option('host', 'default: 0.0.0.0')
-  command.option('port', 'default: 4000')
+  command.option('host', 'Specify server host. (default: 0.0.0.0)')
+  command.option('port', 'Specify server port. (default: 4000)')
 
   if (api.command === 'dev') {
     api.chainWebpack(config => {
       config.output.filename('[name].js')
-
-      config
-        .entry('app')
-        .add('webpack-dev-server/client?/')
-        .add('webpack/hot/dev-server')
-
-      config.plugin('hmr').use(webpack.HotModuleReplacementPlugin)
     })
   }
 }

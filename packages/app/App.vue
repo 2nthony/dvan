7c9ed64aa238f4c624d93f5400f0a1d1f2946a72
evila <ijoec123@gmail.com>
2018-11-10 02:22:04 +0800

    [app] Render App.vue to instead of render virual dom

---
+++ packages/app/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+body {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+</style>
--- packages/app/createApp.js
+++ packages/app/createApp.js
@@ -1,22 +1,15 @@
 import Vue from 'vue'
 import createRouter from './router'
+import _App from './App.vue'
 
-export default () => {
+export default ({
+  App
+} = {}) => {
   const router = createRouter()
 
   const app = new Vue({
     router,
-    render: h => h(
-      'div',
-      {
-        attrs: {
-          id: 'app'
-        }
-      },
-      [
-        h('router-view')
-      ]
-    )
+    render: h => h(App || _App)
   })
 
   return { app, router }

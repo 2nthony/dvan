768dcc16b04d8e470b90702b976c39ab9edaceea
evila <ijoec123@gmail.com>
2018-11-14 21:33:30 +0800

    [app] support user-404 page instead of default 404 page

--- packages/app/router.js
+++ packages/app/router.js
@@ -7,6 +7,8 @@ import { routes } from '@modules/vue-auto-routes'
 Vue.use(Router)
 
 export default () => {
+  let NotFound
+
   const router = new Router({
     mode: __ROUTER_MODE__,
     base: __PUBLIC_PATH__,
@@ -16,13 +18,20 @@ export default () => {
       }
       return { x: 0, y: 0 }
     },
+
     routes: [
-      ...routes,
-      {
-        path: '*',
-        name: '404',
+      ...(routes.filter(route => {
+        if (route.path !== '/404') {
+          return route
+        } else {
+          NotFound = route
+        }
+      })),
+
+      // 404
+      Object.assign({
         component: () => import(/* webpackChunkName: "404" */ './404.vue')
-      }
+      }, NotFound, { path: '*' })
     ]
   })
 

47f663d9987ac8a9f01049572e353da4b5c6d60c
evila <ijoec123@gmail.com>
2018-11-17 22:38:05 +0800

    [app] enhanceApp

--- packages/app/createApp.js
+++ packages/app/createApp.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import createRouter from './router'
 
+const r = require.context('@app/enhanceApp', false, /\.m?js$/)
+const enhances = new Set()
+r.keys().forEach(fp => {
+  if (r(fp).default) {
+    enhances.add(r(fp).default)
+  }
+})
+
 export default ({ App } = {}) => {
   const router = createRouter()
 
@@ -8,6 +16,10 @@ export default ({ App } = {}) => {
     router,
     render: h => h(App || require('./App.vue').default)
   })
+
+  if (enhances.size > 0) {
+    enhances.forEach(enhanceApp => enhanceApp({ Vue, app, router }))
+  }
 
   return { app, router }
 }

a30e8ddcb0bb4cf7b49de64c6ef08025d9196a60
evila <ijoec123@gmail.com>
2018-11-17 22:34:51 +0800

    chunk deps

--- packages/app/index.js
+++ packages/app/index.js
@@ -6,6 +6,26 @@ exports.extend = api => {
   const { html, pagesDir } = api.config
 
   api.chainWebpack(config => {
+    if (api.mode === 'production') {
+      config.optimization.splitChunks({
+        cacheGroups: {
+          vendors: {
+            name: `chunk-vendors`,
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+            chunks: 'initial'
+          },
+          common: {
+            name: `chunk-common`,
+            minChunks: 2,
+            priority: -20,
+            chunks: 'initial',
+            reuseExistingChunk: true
+          }
+        }
+      })
+    }
+
     config.entry('app').add(path.join(__dirname, 'client.entry'))
 
     config.resolve.alias

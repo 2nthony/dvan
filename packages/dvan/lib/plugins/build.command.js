50aa4025e4abab268c8d268553e04e51847cc67c
evila <ijoec123@gmail.com>
2018-11-28 17:23:05 +0800

    [dvan] Update help text

--- packages/dvan/lib/plugins/build.command.js
+++ packages/dvan/lib/plugins/build.command.js
@@ -4,14 +4,19 @@ const setSharedCLIOptions = require('@dvan/cli-utils/sharedOptions')
 exports.name = 'built-in:build.command'
 
 exports.extend = api => {
-  const command = api.registerCommand('build', 'Build app', async () => {
-    if (api.flags.help) return
-    if (!api.flags.clean) {
-      fs.emptyDirSync(api.resolve(api.config.outDir))
-    }
+  const command = api.registerCommand(
+    'build',
+    'Build App as an SPA.',
+    async () => {
+      if (api.flags.help) return
 
-    await api.compiler(api.resolveWebpackConfig())
-  })
+      if (!api.flags.clean) {
+        await fs.emptyDir(api.resolve(api.config.outDir))
+      }
+
+      await api.compiler(api.resolveWebpackConfig())
+    }
+  )
 
   setSharedCLIOptions(command)
   command
--- packages/dvan/lib/plugins/dev.command.js
+++ packages/dvan/lib/plugins/dev.command.js
@@ -5,35 +5,39 @@ exports.name = 'built-in:dev.command'
 exports.extend = api => {
   const webpack = require('webpack')
 
-  const command = api.registerCommand('dev', 'Develop mode', async () => {
-    if (api.flags.help) return
+  const command = api.registerCommand(
+    'dev',
+    'Run App as develope mode.',
+    async () => {
+      if (api.flags.help) return
 
-    api.chainWebpack(config => {
-      config
-        .entry('app')
-        .add('webpack-dev-server/client?/')
-        .add('webpack/hot/dev-server')
-      config.plugin('hmr').use(webpack.HotModuleReplacementPlugin)
-    })
+      api.chainWebpack(config => {
+        config
+          .entry('app')
+          .add('webpack-dev-server/client?/')
+          .add('webpack/hot/dev-server')
+        config.plugin('hmr').use(webpack.HotModuleReplacementPlugin)
+      })
 
-    const { host, port } = api.config.devServer
-    const actualPort = await require('get-port')({ host, port })
-    const WebpackDevServer = require('webpack-dev-server')
-    const server = new WebpackDevServer(
-      webpack(api.resolveWebpackConfig()),
-      Object.assign(
-        {
-          noInfo: true,
-          historyApiFallback: true,
-          overlay: true,
-          hot: true
-        },
-        api.config.devServer
+      const { host, port } = api.config.devServer
+      const actualPort = await require('get-port')({ host, port })
+      const WebpackDevServer = require('webpack-dev-server')
+      const server = new WebpackDevServer(
+        webpack(api.resolveWebpackConfig()),
+        Object.assign(
+          {
+            noInfo: true,
+            historyApiFallback: true,
+            overlay: true,
+            hot: true
+          },
+          api.config.devServer
+        )
       )
-    )
 
-    server.listen(actualPort, host)
-  })
+      server.listen(actualPort, host)
+    }
+  )
 
   setSharedCLIOptions(command)
   command

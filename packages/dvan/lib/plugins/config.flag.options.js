53e869b828a75190f70c4b51d5192e122c988947
evila <ijoec123@gmail.com>
2018-12-11 01:09:06 +0800

    Short flag for `srcDir` and `outDir`

--- packages/dvan/lib/plugins/config.flag.options.js
+++ packages/dvan/lib/plugins/config.flag.options.js
@@ -3,8 +3,8 @@ exports.name = 'built-in:config.flag.options'
 exports.extend = api => {
   api.hook('onInitCLI', ({ command }) => {
     const cmd = require('../utils/easyCmdOption')(command)
-    cmd('--src-dir <dir>', 'Source directory', 'src')
-    cmd('--out-dir <dir>', 'Output directory', '__dist')
+    cmd('-s, --src-dir <dir>', 'Source directory', 'src')
+    cmd('-d, --out-dir <dir>', 'Output directory', '__dist')
     cmd('--public-path <path>', 'Public path', '/')
     cmd('--constants', 'Global constants')
     cmd('--evergreen', 'Only targeting evergreen browsers', false)

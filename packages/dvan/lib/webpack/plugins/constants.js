64a0a2760c7250ec66aa50f97c887d61dd74e3b6
evila <ijoec123@gmail.com>
2019-01-04 15:58:38 +0800

    Add `__IS_VUE__` to constants

--- packages/dvan/lib/webpack/plugins/constants.js
+++ packages/dvan/lib/webpack/plugins/constants.js
@@ -3,7 +3,8 @@ module.exports = (config, api) => {
     Object.assign(
       {
         __PUBLIC_PATH__: JSON.stringify(api.config.publicPath),
-        __DVAN_APP__: JSON.stringify(process.env.DVAN_APP)
+        __DVAN_APP__: JSON.stringify(process.env.DVAN_APP),
+        __IS_VUE__: JSON.stringify(process.env.DVAN_APP === 'vue')
       },
       api.config.constants
     )

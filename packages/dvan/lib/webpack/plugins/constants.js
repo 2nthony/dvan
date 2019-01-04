1c055fb0960056d3ff87d5506f5b91c12ee6ad74
evila <ijoec123@gmail.com>
2019-01-04 15:55:48 +0800

    Use `__DVAN_APP__` in client side.

--- packages/dvan/lib/webpack/plugins/constants.js
+++ packages/dvan/lib/webpack/plugins/constants.js
@@ -2,7 +2,8 @@ module.exports = (config, api) => {
   config.plugin('constants').use(require('webpack').DefinePlugin, [
     Object.assign(
       {
-        __PUBLIC_PATH__: JSON.stringify(api.config.publicPath)
+        __PUBLIC_PATH__: JSON.stringify(api.config.publicPath),
+        __DVAN_APP__: JSON.stringify(process.env.DVAN_APP)
       },
       api.config.constants
     )

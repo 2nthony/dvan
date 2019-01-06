a1683a8f522628803e183278257732af4321bb02
evila <ijoec123@gmail.com>
2019-01-06 21:46:52 +0800

    Remove @params for webpack-chain

--- packages/dvan/lib/plugins/config/css.js
+++ packages/dvan/lib/plugins/config/css.js
@@ -5,149 +5,139 @@ exports.extend = api => {
     require('../shared/extractCssOptions')(api, command)
   })
 
-  api.hook(
-    'onCreateWebpackConfig',
-    /**
-     * @param {import('webpack-chain')} config
-     */
-    (config, { type }) => {
-      const {
-        loaderOptions,
-        extractCss: shouldExtract,
-        sourceMap,
-        minimize
-      } = api.config
-      const shouldMinimize = Boolean(minimize)
-      const isServer = type === 'server'
+  api.hook('onCreateWebpackConfig', (config, { type }) => {
+    const {
+      loaderOptions,
+      extractCss: shouldExtract,
+      sourceMap,
+      minimize
+    } = api.config
+    const shouldMinimize = Boolean(minimize)
+    const isServer = type === 'server'
 
-      const hasPostcssConfig = require('../../utils/loadConfig')({
-        files: ['package.json'],
-        matches: ['.postcssrc*', 'postcss.config.*'],
-        dir: api.cwd,
-        packageKey: 'postcss'
-      })
+    const hasPostcssConfig = require('../../utils/loadConfig')({
+      files: ['package.json'],
+      matches: ['.postcssrc*', 'postcss.config.*'],
+      dir: api.cwd,
+      packageKey: 'postcss'
+    })
 
-      if (hasPostcssConfig.configPath) {
-        api.logger.debug(
-          'Applying postcss config at',
-          require('../../utils/colorfulFile')(
-            require('path').relative(api.cwd, hasPostcssConfig.configPath)
-          )
+    if (hasPostcssConfig.configPath) {
+      api.logger.debug(
+        'Applying postcss config at',
+        require('../../utils/colorfulFile')(
+          require('path').relative(api.cwd, hasPostcssConfig.configPath)
         )
-      }
+      )
+    }
 
-      const extractOptions = {
-        filename: api.config.output.fileNames.css,
-        chunkFilename: api.config.output.fileNames.css.replace(
-          /\.css$/,
-          '.chunk.css'
-        )
-      }
+    const extractOptions = {
+      filename: api.config.output.fileNames.css,
+      chunkFilename: api.config.output.fileNames.css.replace(
+        /\.css$/,
+        '.chunk.css'
+      )
+    }
 
-      const cssnanoOptions = {
-        safe: true,
-        autoprefixer: { disable: true },
-        mergeLonghand: false
-      }
+    const cssnanoOptions = {
+      safe: true,
+      autoprefixer: { disable: true },
+      mergeLonghand: false
+    }
 
-      if (sourceMap) {
-        cssnanoOptions.map = { inline: false }
-      }
+    if (sourceMap) {
+      cssnanoOptions.map = { inline: false }
+    }
 
-      const createCSSRule = (lang, test, loader, options) => {
-        const applyLoader = (rule, modules = false) => {
-          if (!isServer) {
-            if (shouldExtract) {
-              rule
-                .use('extract-css-loader')
-                .loader(require('mini-css-extract-plugin').loader)
-            } else {
-              rule
-                .use('vue-style-loader')
-                .loader('vue-style-loader')
-                .options({ sourceMap })
-            }
+    const createCSSRule = (lang, test, loader, options) => {
+      const applyLoader = (rule, modules = false) => {
+        if (!isServer) {
+          if (shouldExtract) {
+            rule
+              .use('extract-css-loader')
+              .loader(require('mini-css-extract-plugin').loader)
+          } else {
+            rule
+              .use('vue-style-loader')
+              .loader('vue-style-loader')
+              .options({ sourceMap })
           }
+        }
 
+        rule
+          .use('css-loader')
+          .loader(isServer ? 'css-loader/locals' : 'css-loader')
+          .options(
+            Object.assign(
+              {
+                modules,
+                sourceMap,
+                importLoaders: 1 + (hasPostcssConfig ? 1 : 0),
+                localIdentName: api.isProd
+                  ? '[local]_[hash:base64:6]'
+                  : '[path][name]__[local]--[hash:base64:6]'
+              },
+              loaderOptions.css
+            )
+          )
+
+        if (loader) {
           rule
-            .use('css-loader')
-            .loader(isServer ? 'css-loader/locals' : 'css-loader')
+            .use(loader)
+            .loader(loader)
             .options(
               Object.assign(
-                {
-                  modules,
-                  sourceMap,
-                  importLoaders: 1 + (hasPostcssConfig ? 1 : 0),
-                  localIdentName: api.isProd
-                    ? '[local]_[hash:base64:6]'
-                    : '[path][name]__[local]--[hash:base64:6]'
-                },
-                loaderOptions.css
+                { sourceMap },
+                options,
+                loaderOptions[loader.replace('-loader', '')]
               )
             )
-
-          if (loader) {
-            rule
-              .use(loader)
-              .loader(loader)
-              .options(
-                Object.assign(
-                  { sourceMap },
-                  options,
-                  loaderOptions[loader.replace('-loader', '')]
-                )
-              )
-          }
-        }
-
-        const baseRule = config.module.rule(lang).test(test)
-
-        const vueModuleRule = baseRule
-          .oneOf('vue-modules')
-          .resourceQuery(/module/)
-        applyLoader(vueModuleRule, true)
-
-        const vueNormalRule = baseRule
-          .oneOf('vue-normal')
-          .resourceQuery(/\?vue/)
-        applyLoader(vueNormalRule)
-
-        const moduleRule = baseRule
-          .oneOf('normal-modules')
-          .test(/\.module\.\w+$/)
-        applyLoader(moduleRule, true)
-
-        const normalRule = baseRule.oneOf('normal')
-        applyLoader(normalRule)
-      }
-
-      if (shouldExtract) {
-        config
-          .plugin('extract-css')
-          .use('mini-css-extract-plugin', [extractOptions])
-
-        if (shouldMinimize) {
-          config
-            .plugin('optimize-css')
-            .use(require('@intervolga/optimize-cssnano-plugin'), [
-              {
-                sourceMap,
-                cssnanoOptions
-              }
-            ])
         }
       }
 
-      createCSSRule('css', /\.css$/)
-      createCSSRule('postcss', /\.p(ost)?css$/)
-      createCSSRule('stylus', /\.styl(us)?$/, 'stylus-loader', {
-        preferPathResolve: 'webpack'
-      })
-      createCSSRule('less', /\.less$/, 'less-loader')
-      createCSSRule('sass', /\.sass$/, 'sass-loader', {
-        indentedSyntax: true
-      })
-      createCSSRule('scss', /\.scss$/, 'sass-loader')
+      const baseRule = config.module.rule(lang).test(test)
+
+      const vueModuleRule = baseRule
+        .oneOf('vue-modules')
+        .resourceQuery(/module/)
+      applyLoader(vueModuleRule, true)
+
+      const vueNormalRule = baseRule.oneOf('vue-normal').resourceQuery(/\?vue/)
+      applyLoader(vueNormalRule)
+
+      const moduleRule = baseRule.oneOf('normal-modules').test(/\.module\.\w+$/)
+      applyLoader(moduleRule, true)
+
+      const normalRule = baseRule.oneOf('normal')
+      applyLoader(normalRule)
     }
-  )
+
+    if (shouldExtract) {
+      config
+        .plugin('extract-css')
+        .use('mini-css-extract-plugin', [extractOptions])
+
+      if (shouldMinimize) {
+        config
+          .plugin('optimize-css')
+          .use(require('@intervolga/optimize-cssnano-plugin'), [
+            {
+              sourceMap,
+              cssnanoOptions
+            }
+          ])
+      }
+    }
+
+    createCSSRule('css', /\.css$/)
+    createCSSRule('postcss', /\.p(ost)?css$/)
+    createCSSRule('stylus', /\.styl(us)?$/, 'stylus-loader', {
+      preferPathResolve: 'webpack'
+    })
+    createCSSRule('less', /\.less$/, 'less-loader')
+    createCSSRule('sass', /\.sass$/, 'sass-loader', {
+      indentedSyntax: true
+    })
+    createCSSRule('scss', /\.scss$/, 'sass-loader')
+  })
 }
--- packages/dvan/lib/plugins/config/vue.js
+++ packages/dvan/lib/plugins/config/vue.js
@@ -5,28 +5,20 @@ exports.name = 'built-in:config-vue'
 exports.extend = api => {
   if (!api.hasDependency('vue')) return
 
-  api.hook(
-    'onCreateWebpackConfig',
-    /**
-     * @param {import('webpack-chain')} config
-     */
-    config => {
-      config.resolve.alias.set('vue$', 'vue/dist/vue.esm')
+  api.hook('onCreateWebpackConfig', config => {
+    config.resolve.extensions.add('.vue')
 
-      config.resolve.extensions.add('.vue')
+    config.module
+      .rule('vue')
+      .test(/\.vue$/)
+      .use('vue-loader')
+      .loader('vue-loader')
+      .options(
+        Object.assign({}, api.config.loaderOptions.vue, {
+          compiler: require(resolveFrom(api.cwd, 'vue-template-compiler'))
+        })
+      )
 
-      config.module
-        .rule('vue')
-        .test(/\.vue$/)
-        .use('vue-loader')
-        .loader('vue-loader')
-        .options(
-          Object.assign({}, api.config.loaderOptions.vue, {
-            compiler: require(resolveFrom(api.cwd, 'vue-template-compiler'))
-          })
-        )
-
-      config.plugin('vue-loader').use(require('vue-loader/lib/plugin'))
-    }
-  )
+    config.plugin('vue-loader').use(require('vue-loader/lib/plugin'))
+  })
 }

82ca5784ddb3edb9ce60a427e9bebc8ddbd70313
evillt <ijoec123@gmail.com>
2019-02-26 13:30:49 +0800

    chore: update pkg generate

--- packages/create-dvan-app/lib/update-pkg.js
+++ packages/create-dvan-app/lib/update-pkg.js
@@ -10,11 +10,11 @@ module.exports = (api, when) => {
       build: 'dvan --prod'
     },
     dependencies: {
-      vue: when(frameworks.includes('vue'), '^2.5.22'),
-      'vue-template-compiler': when(frameworks.includes('vue'), '^2.5.22')
+      vue: when(frameworks.includes('vue'), '^2.5.22')
     },
     devDependencies: {
-      dvan: '^2.3.2'
+      dvan: '^2.3.2',
+      'vue-template-compiler': when(frameworks.includes('vue'), '^2.5.22')
     }
   }
 }

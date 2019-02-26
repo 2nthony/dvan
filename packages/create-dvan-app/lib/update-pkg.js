594fadf870e19f3aac3053ba053523fac0e619bc
evillt <ijoec123@gmail.com>
2019-02-26 21:55:26 +0800

    chore: update project pkg

--- packages/create-dvan-app/lib/update-pkg.js
+++ packages/create-dvan-app/lib/update-pkg.js
@@ -1,20 +1,32 @@
-module.exports = (api, when) => {
-  const { frameworks } = api.answers
+const when = (condition, value, fallback) => (condition ? value : fallback)
 
+module.exports = ({
+  name,
+  description,
+  author,
+  email,
+  frameworks,
+  vueRouter,
+  vueAutoRoutes
+}) => {
   return {
-    name: api.outFolder,
+    name,
+    description,
     private: true,
+    author: `${author} <${email}>`,
     scripts: {
       dvan: 'dvan',
       dev: 'dvan --dev',
       build: 'dvan --prod'
     },
     dependencies: {
-      vue: when(frameworks.includes('vue'), '^2.5.22')
+      vue: when(frameworks.includes('vue'), '^2.6.7'),
+      'vue-router': when(vueRouter, '^3.0.2'),
+      'vue-auto-routes': when(vueAutoRoutes, '^1.3.0')
     },
     devDependencies: {
       dvan: '^2.3.7',
-      'vue-template-compiler': when(frameworks.includes('vue'), '^2.5.22')
+      'vue-template-compiler': when(frameworks.includes('vue'), '^2.6.7')
     }
   }
 }

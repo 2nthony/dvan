d971b3dd9d0c7d25e21c87b0551a7b5c1260f6c3
evillt <ijoec123@gmail.com>
2019-02-26 23:30:58 +0800

    chore: update App.vue with main template

--- packages/create-dvan-app/generator/templates/vue/main/src/App.vue
+++ packages/create-dvan-app/generator/templates/vue/main/src/App.vue
@@ -14,3 +14,9 @@ export default {
   }
 }
 </script>
+
+<style>
+#app {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+</style>

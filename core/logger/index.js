3b58a46fafaf2fa60c07b85010ceb3f2cda7dae3
evillt <ijoec123@gmail.com>
2019-03-16 16:00:20 +0800

    chore: split logger to a standalone package

---
+++ core/logger/index.js
@@ -0,0 +1,59 @@
+const chalk = require('chalk')
+
+class Logger {
+	constructor(opts) {
+		this.setOptions(opts)
+	}
+
+	setOptions(opts) {
+		this.opts = Object.assign({}, this.opts, opts)
+	}
+
+	log(...args) {
+		console.log(
+			...args.map(arg => {
+				return typeof arg === 'function' ? arg() : arg
+			})
+		)
+	}
+
+	debug(...args) {
+		if (!this.opts.debug) {
+			return
+		}
+		this.log(chalk.magenta('debug'), ...args)
+	}
+
+	error(...args) {
+		this.log(chalk.bold.hex('#fd6f6b')('error', ...args))
+		process.exitCode = process.exitCode || 1
+	}
+
+	warn(...args) {
+		this.log(chalk.yellow('warning'), ...args)
+		process.exitCode = process.exitCode || 1
+	}
+
+	success(...args) {
+		this.log(chalk.green('success'), ...args)
+	}
+
+	done(...args) {
+		this.log(
+			chalk.bold.green(process.platform === 'win32' ? '√' : '✔', ...args)
+		)
+	}
+
+	tips(...args) {
+		this.log(chalk.cyan('tips'), ...args)
+	}
+
+	color(type, ...args) {
+		if (type.startsWith('#')) {
+			return this.log(chalk.hex(type)(...args))
+		}
+		return this.log(chalk[type](...args))
+	}
+}
+
+module.exports = new Logger()

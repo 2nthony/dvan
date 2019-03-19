a1b634c6a5b840fc559362a47fef86b137e9a4f1
evillt <ijoec123@gmail.com>
2019-03-19 14:16:43 +0800

    fix: hot-reload with serve

--- core/dvan/lib/plugins/shared/webpackDevConfig.js
+++ core/dvan/lib/plugins/shared/webpackDevConfig.js
@@ -1,17 +1,17 @@
 module.exports = (api, config) => {
-	if (!api.cli.options.dev) return
+  if (!api.cli.options.serve) return
 
-	const { hotEntries = ['index'], hot } = api.config.devServer || {}
+  const { hotEntries = ['index'], hot } = api.config.devServer || {}
 
-	config.output.filename('[name].js')
+  config.output.filename('[name].js')
 
-	if (hot) {
-		for (const entry of hotEntries) {
-			if (config.entryPoints.has(entry)) {
-				config.entry(entry).prepend('#webpack-hot-client')
-			}
-		}
+  if (hot) {
+    for (const entry of hotEntries) {
+      if (config.entryPoints.has(entry)) {
+        config.entry(entry).prepend('#webpack-hot-client')
+      }
+    }
 
-		config.plugin('hot').use(require('webpack').HotModuleReplacementPlugin)
-	}
+    config.plugin('hot').use(require('webpack').HotModuleReplacementPlugin)
+  }
 }

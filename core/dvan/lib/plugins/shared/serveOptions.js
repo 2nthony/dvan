0af6e72aacbf7233d6e091e9ac7678734195e68a
evillt <ijoec123@gmail.com>
2019-03-21 11:12:53 +0800

    chore: rename

--- core/dvan/lib/plugins/shared/serveOptions.js
+++ core/dvan/lib/plugins/shared/serveOptions.js
@@ -1,8 +1,8 @@
 module.exports = command => {
   const cmd = require('../../utils/easyCmdOption')(command)
-  cmd('--host <host>', 'Development server host', '0.0.0.0')
-  cmd('--port <port>', 'Development server port', 4000)
-  cmd('--hot', 'Hot reload', true)
+  cmd('--host <host>', 'Serve host', '0.0.0.0')
+  cmd('-p, --port <port>', 'Serve port', '4000')
   cmd('-o, --open', 'Open in browser')
+  cmd('--no-hot', 'Disable hot reloading')
   cmd('--local', 'Alias for --host localhost')
 }

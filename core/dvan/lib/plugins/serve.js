15986b54504addd878a6b8fa5335d4f104154db5
evillt <ijoec123@gmail.com>
2019-03-21 11:15:24 +0800

    chore(serve): use shared serve options

--- core/dvan/lib/plugins/serve.js
+++ core/dvan/lib/plugins/serve.js
@@ -6,12 +6,7 @@ exports.extend = api => {
 
     if (!api.isServe) return
 
-    const cmd = require('../utils/easyCmdOption')(command)
-    cmd('--host <host>', 'Serve host', '0.0.0.0')
-    cmd('-p, --port <port>', 'Serve port', '4000')
-    cmd('-o, --open', 'Open in browser')
-    cmd('--no-hot', 'Disable hot reloading')
-    cmd('--local', 'Alias for --host localhost')
+    require('./shared/serveOptions')(command)
 
     command.action(async () => {
       api.logger.debug('Starting server...')

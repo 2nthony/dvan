a6cc9a111d29df2e1559cd6880dbc4e6fe10786a
evillt <ijoec123@gmail.com>
2019-03-22 13:25:02 +0800

    feat: show memory usage

--- core/dev-utils/printServeMessage.js
+++ core/dev-utils/printServeMessage.js
@@ -1,22 +1,22 @@
 const chalk = require('chalk')
 const logger = require('@dvan/logger')
+const prettyBytes = require('./prettyBytes')
 
-module.exports = (host, port) =>
+module.exports = ({ host, port }) =>
   class PrintServeMessage {
     apply(compiler) {
       compiler.hooks.done.tap('print-serve-message', stats => {
         if (stats.hasErrors() || stats.hasWarnings()) return
 
+        const { heapUsed } = process.memoryUsage()
+        const ip = require('ip').address()
+
         logger.log()
         logger.success('Running server with')
-        logger.log(
-          `Local:\t\t\t${chalk.underline(`http://${host}:${chalk.bold(port)}`)}`
-        )
-        logger.log(
-          `On your network:\t${chalk.underline(
-            `http://${require('ip').address()}:${chalk.bold(port)}`
-          )}`
-        )
+        logger.log(`Local:             ${`http://${host}:${chalk.bold(port)}`}`)
+        logger.log(`On Your Network:   ${`http://${ip}:${chalk.bold(port)}`}`)
+        logger.log()
+        logger.log(chalk.dim(`> ${prettyBytes(heapUsed)} memory used`))
         logger.log()
       })
     }

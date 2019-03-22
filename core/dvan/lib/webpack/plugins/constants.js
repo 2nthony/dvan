d40679ba26fcfef5c3548315d5d3d23c2a1edacf
evillt <ijoec123@gmail.com>
2019-03-22 11:26:13 +0800

    chore: rename to PUBLIC_URL for constants

--- core/dvan/lib/webpack/plugins/constants.js
+++ core/dvan/lib/webpack/plugins/constants.js
@@ -2,7 +2,7 @@ module.exports = (config, api) => {
   config.plugin('constants').use(require('webpack').DefinePlugin, [
     Object.assign(
       {
-        PUBLIC_PATH: JSON.stringify(api.config.output.publicUrl),
+        PUBLIC_URL: JSON.stringify(api.config.output.publicUrl),
         DVAN_APP: JSON.stringify(process.env.DVAN_APP),
         IS_VUE: JSON.stringify(process.env.DVAN_APP === 'vue')
       },
